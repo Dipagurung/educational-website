1ccf71ef8d9beaf89a00563df2eb47a7c000b455
Dipagurung <chamsgurung@gmail.com>
2021-06-04 14:14:22 +0545

    my first commit

---
+++ .js
@@ -0,0 +1,8 @@
+var navLinks = document.getElementById("nav-links")
+
+function showMenu(){
+    navLinks.style.right = "0";  //To change the right menu
+}
+function hideMenu(){
+    navLinks.style.right = "-200px"; //Add right value -200px
+}
